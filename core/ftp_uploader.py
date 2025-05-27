853c8144fbd078f5038441227447e0d285dde132
Marcio Lorran <marcio.lorran@gmail.com>
2025-05-27 20:13:50 -0300

    remove sanitization

--- core/ftp_uploader.py
+++ core/ftp_uploader.py
@@ -2,14 +2,6 @@ import os
 from ftplib import FTP
 import re
 
-def sanitize_fatx_name(name, max_length=42):
-    name = re.sub(r'[<>:"/\\|?*\+=\[\];,\.]', '', name)
-    name = name.replace(' ', '_')
-    name = re.sub(r'_+', '_', name)
-    name = name.strip('_')
-
-    return name[:max_length]
-
 def ensure_ftp_dirs(ftp, path, logger):
     dirs = path.strip("/").split("/")
     for d in dirs:
@@ -48,7 +40,9 @@ def upload_directory(local_path, remote_path, ip, logger, port=21, user="xbox", 
     for root, _, files in os.walk(local_path):
         rel_path = os.path.relpath(root, local_path)
         rel_path = rel_path.replace("\\", "/")
-        rel_path = sanitize_fatx_name(rel_path)
+        rel_path = rel_path.replace(" ", "_")
+        rel_path = "".join(c for c in rel_path if c.isalnum() or c in ('.', '_', '/'))
+        rel_path = rel_path.replace("__", "_")
         logger(f"Preparing to upload files from: {rel_path}")
 
         # Garantee the remote directory exists
